74d453b7dc9d77184051b874243f7ed05ac69fde
Michał Januszewski s22882 <s22882@pjwstk.edu.pl>
2023-02-15 14:39:33 +0100

    Usuwanie, modyfikacja Restauracji, refaktoryzacja kontrolera (service)

--- RestaurantAPI/RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/RestaurantAPI/Controllers/RestaurantController.cs
@@ -1,60 +1,79 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using RestaurantAPI.Entities;
 using RestaurantAPI.Models;
+using RestaurantAPI.Services;
 
 namespace RestaurantAPI.Controllers
 {
     [Route("api/restaurant")]
     public class RestaurantController : ControllerBase
     {
-        private readonly RestaurantDbContext _dbContext;
-        private readonly IMapper _mapper;
-        public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
+        private readonly IRestaurantService _service;
+        public RestaurantController(IRestaurantService service)
         {
-            _dbContext = dbContext;
-            _mapper = mapper;
+            _service = service;
         }
         [HttpGet]
         public ActionResult <IEnumerable<RestaurantDto>> GetAll()
         {
-            var restaurants = _dbContext
-                .Restaurants
-                .Include(r => r.Address)
-                .Include(r => r.Dishes)
-                .ToList();
-
-            var restaurantDtos = _mapper.Map<List<RestaurantDto>>(restaurants);
+            var restaurantDtos = _service.GetAll();
             return Ok(restaurantDtos);
         }
 
         [HttpGet("{id}")]
         public ActionResult<RestaurantDto> Get([FromRoute] int id)
         {
-            var restaurant = _dbContext
-                .Restaurants
-                .Include(r => r.Address)
-                .Include(r => r.Dishes)
-                .FirstOrDefault(x => x.Id == id);
+            var restaurant = _service.GetById(id);
             if (restaurant is null)
             {
                 return NotFound();
             }
-            var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
-            return Ok(restaurantDto);
+            return Ok(restaurant);
         }
 
         [HttpPost]
         public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
         {
-            var newRestaurant = _mapper.Map<Restaurant>(dto);
-            _dbContext.Restaurants.Add(newRestaurant);
-            _dbContext.SaveChanges();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            int id = _service.Create(dto);
             // przekazujemy odpowiedz Created wraz ze sciezka pod ktora bedzie dostepny utworzony zasób
-            return Created($"/api/restaurant/{newRestaurant.Id}", null);
+            return Created($"/api/restaurant/{id}", null);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteRestaurant([FromRoute] int id)
+        {
+            var isDeleted = _service.Delete(id);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult UpdateRestaurant([FromRoute] int id, [FromBody] UpdateRestaurantDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var isUpdated = _service.Update(id, dto);
+
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+
         }
     }
 }
---
+++ RestaurantAPI/RestaurantAPI/Services/RestaurantService.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Services
+{
+    public interface IRestaurantService
+    {
+        RestaurantDto GetById(int id);
+        IEnumerable<RestaurantDto> GetAll();
+        int Create(CreateRestaurantDto dto);
+        bool Delete(int id);
+        bool Update(int id, UpdateRestaurantDto dto);
+    }
+
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly RestaurantDbContext _context;
+        private readonly IMapper _mapper;
+
+        public RestaurantService(RestaurantDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public RestaurantDto GetById(int id)
+        {
+            var restaurant = _context
+                .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant == null)
+            {
+                return null;
+            }
+
+            var result = _mapper.Map<RestaurantDto>(restaurant);
+            return result;
+        }
+
+        public IEnumerable<RestaurantDto> GetAll()
+        {
+            var restaurants = _context
+                .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
+                .ToList();
+
+            return _mapper.Map<List<RestaurantDto>>(restaurants);
+        }
+
+        public int Create(CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant.Id;
+        }
+
+        public bool Delete(int id)
+        {
+            var restaurant = _context.Restaurants
+                .FirstOrDefault(r => r.Id == id);
+            if (restaurant is null)
+            {
+                return false;
+            }
+
+            _context.Restaurants.Remove(restaurant);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Update(int id, UpdateRestaurantDto dto)
+        {
+            var restaurant = _context.Restaurants
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant is null)
+            {
+                return false;
+            }
+
+            restaurant.Name = dto.Name;
+            restaurant.Description = dto.Description;
+            restaurant.HasDelivery = dto.HasDelivery;
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
