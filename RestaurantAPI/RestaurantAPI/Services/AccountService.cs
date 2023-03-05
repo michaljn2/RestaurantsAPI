58844b86f6ccd1bdcdbaa00e68c9d86522579f00
Michał Januszewski s22882 <s22882@pjwstk.edu.pl>
2023-03-05 19:44:13 +0100

    Dodanie hashowania hasla i walidacji rejestracji uzytkownika

--- RestaurantAPI/RestaurantAPI/Services/AccountService.cs
+++ RestaurantAPI/RestaurantAPI/Services/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantAPI.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using RestaurantAPI.Entities;
 using RestaurantAPI.Models;
 
 namespace RestaurantAPI.Services
@@ -10,10 +11,13 @@ namespace RestaurantAPI.Services
     public class AccountService : IAccountService
     {
         private readonly RestaurantDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
 
-        public AccountService(RestaurantDbContext context)
+
+        public AccountService(RestaurantDbContext context, IPasswordHasher<User> passwordHasher)
         {
             _context = context;
+            _passwordHasher = passwordHasher;
         }
         public void RegisterUser(RegisterUserDto dto)
         {
@@ -24,6 +28,8 @@ namespace RestaurantAPI.Services
                 Nationality = dto.Nationality,
                 RoleId = dto.RoleId,
             };
+            var hashedPassword = _passwordHasher.HashPassword(user, dto.Password);
+            user.PasswordHash = hashedPassword;
             _context.Users.Add(user);
             _context.SaveChanges();
 
